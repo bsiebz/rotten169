26796e59437083a979392f809b1ca2e00b93b5b6
stvnwu <wucommasteven@gmail.com>
2015-02-17 17:45:46 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = '5f1175c0034c8ea4a9cfac296fc9545f2a366b36f32bba9f75d2e9c2f170441fb472e487e27f1b647bcfe1b88fa5e594fe41af300a01309c9ea5541b0ce98eff'
