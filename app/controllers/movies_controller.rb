9f94ffc8e9b923906ed8b50c814780970b287cd3
Steven Wu <wucommasteven@gmail.com>
2015-02-17 21:14:36 -0800

    default to all checked

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,15 +7,14 @@ class MoviesController < ApplicationController
   end
 
   def index
-    if params[:title_sort] == 'true'
-      @movies = Movie.find(:all, :order => "title")
-    elsif params[:date_sort] == 'true'
-      @movies = Movie.find(:all, :order => "release_date")
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+    @selected_ratings = (params[:ratings].present? ? params[:ratings].keys : @all_ratings)
+    if params.key?(:title_sort)
+      @movies = Movie.order(:title).where(:rating => @selected_ratings)
+    elsif params.key?(:sort_release_date)
+      @movies = Movie.order(:release_date).where(:rating => @selected_ratings)
     else
-      @movies = Movie.all
-    end
-    @all_ratings = Movie.uniq.pluck(:rating)
-    @selected_ratings = ['G', 'PG']
+      @movies = Movie.where(:rating => @selected_ratings)
   end
 
   def new
