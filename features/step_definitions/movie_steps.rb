896b12d3c5350c656e69160263a677afcffd29f1
Steven Wu <wucommasteven@gmail.com>
2015-02-23 17:35:15 -0800

    testing

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,7 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-  Movie.create!({:title => movie["title"], :rating => movie["rating"], :release_date => movie["release_date"]})
+    Movie.create!({:title => movie["title"], :rating => movie["rating"], :release_date => movie["release_date"]})
   end
 end
 
@@ -25,23 +25,8 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  rating_list.split(',').each do |rating|
-    if uncheck
-      uncheck("ratings_#{rating}")
-    else
-      check("ratings_#{rating}")
-    end
-  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  Movie.find(:all).each do |movie|
-    title = movie["title"]
-    if page.respond_to? :should
-      page.should have_content(title)
-    else
-      assert page.has_content?(title)
-    end
-  end
 end
