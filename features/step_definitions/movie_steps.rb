95ecd851c3ffb1cd4b6fa0d3fb98563848c2a9a0
Steven Wu <wucommasteven@gmail.com>
2015-02-23 18:03:08 -0800

    what

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,7 +4,7 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    Movie.create!({:title => movie["title"], :rating => movie["rating"], :release_date => movie["release_date"]})
+    Movie.create movie
   end
 end
 
@@ -14,7 +14,15 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  movies = Movie.all
+  
+  if movies.size == 10
+    movies.each do |movie|
+      assert page.body =~ /#{movie.title}/m, "#{movie.title} did not appear"
+    end
+  else
+    false
+  end
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,8 +33,19 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(',').each do |rating|
+    if uncheck
+      uncheck "ratings_#{rating}"
+    else
+      check "ratings_#{rating}"
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
+  movies = Movie.all
+  movies.each do |movie|
+    assert true unless page.body =~ /#{movie.title}/m
+  end
 end
