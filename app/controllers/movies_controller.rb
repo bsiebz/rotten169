0ca88f4079e36a54b03b8bf63210564d221eaf5e
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-03-02 22:43:10 -0800

    Didn't follow cuke well enough first time..

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -54,7 +54,7 @@ class MoviesController < ApplicationController
   def similar
     @movie = Movie.find params[:id]
     if (@movie.director.length == 0)
-      flash[:notice] = "'#{@movie.title}' does not contain director name."
+      flash[:notice] = "'#{@movie.title}' has no director info"
       redirect_to movies_path
     end
     @movies = Movie.find_all_by_director(@movie.director)
